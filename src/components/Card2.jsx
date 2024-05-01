0b58b3031f1354363ab8ae03a2c310a097427eed
jusish <ishimwejustin67@outlook.com>
2024-05-01 12:22:19 +0200

    created card 2

---
+++ src/components/Card2.jsx
@@ -0,0 +1,16 @@
+
+
+const Card2 = () => {
+  return (
+    <div className="flex flex-row">
+      <div className="text-start flex flex-col gap-5">
+        <h2>The Books You Need to Read in 2023</h2>
+        <img src="/src/assets/Line.svg" className=" w-[20%]" alt="" />
+        <p className="text-[#e0e0e0] opacity-[52%] text-[14px] w-[394px]">This is the blog we know you have all been waiting for. We present the top 10 titles for 2023 in fiction, non-fiction and children s books; a glorious mix of masterful storytelling, compelling subject matter and page-turning thrills...  </p>
+      </div>
+      <img src="/src/assets/Book Store/Photo news.png" className=" w-[123px]" alt="" />
+    </div>
+  )
+}
+
+export default Card2
