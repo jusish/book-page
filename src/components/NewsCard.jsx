be44f3a331baa1d58cfdfda9a0e21497c2185677
jusish <ishimwejustin67@outlook.com>
2024-05-01 12:23:33 +0200

    News card created

---
+++ src/components/NewsCard.jsx
@@ -0,0 +1,15 @@
+import MultipleItems3 from "./Slider3"
+
+
+const News = () => {
+  return (
+    <div className=" mt-20 text-start flex flex-col gap-10">
+      <h2>News</h2>
+      <div>
+        <MultipleItems3/>
+      </div>
+    </div>
+  )
+}
+
+export default News
