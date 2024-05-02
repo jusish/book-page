31a4f0300df8c19ee34d2c78b8d754fbf18e019d
jusish <ishimwejustin67@outlook.com>
2024-05-02 21:54:49 +0200

    Card 2 updates

--- src/components/Card2.jsx
+++ src/components/Card2.jsx
@@ -2,13 +2,13 @@
 
 const Card2 = () => {
   return (
-    <div className="flex flex-row">
-      <div className="text-start flex flex-col gap-5">
-        <h2>The Books You Need to Read in 2023</h2>
+    <div className="grid grid-flow-col mx-4">
+      <div className="text-start flex flex-col gap-4">
+        <h2 className=" font-medium text-[20px]">The Books You Need to Read in 2023</h2>
         <img src="/src/assets/Line.svg" className=" w-[20%]" alt="" />
-        <p className="text-[#e0e0e0] opacity-[52%] text-[14px] w-[394px]">This is the blog we know you have all been waiting for. We present the top 10 titles for 2023 in fiction, non-fiction and children s books; a glorious mix of masterful storytelling, compelling subject matter and page-turning thrills...  </p>
+        <p className="text-[#e0e0e0] opacity-[52%] text-[14px]">This is the blog we know you have all been waiting for. We present the top 10 titles for 2023 in fiction, non-fiction and children s books; a glorious mix of masterful storytelling, compelling subject matter and page-turning thrills...  </p>
       </div>
-      <img src="/src/assets/Book Store/Photo news.png" className=" w-[123px]" alt="" />
+      <img src="/src/assets/Book Store/Photo news.png" alt="" />
     </div>
   )
 }
