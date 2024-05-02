01b1725cf4f74fd9a1134e496ce0d6049ce5fc37
jusish <ishimwejustin67@outlook.com>
2024-05-02 21:54:57 +0200

    Hero section updated

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,11 +2,11 @@
 
 const Hero = () => {
   return (
-    <div className="flex flex-row gap-12">
+    <div className="grid justify-center grid-flow-col grid-cols-1 mx-10">
       <div className=" flex flex-col text-start max-w-[500px] gap-6">
-        <h1 className="font-bold text-3xl">New Released this week</h1>
+        <h1 className="text-3xl font-bold">New Released this week</h1>
         <p>It is time to update your reading list with some of the latest and greatest releases in the literary world. From heart-pumping thrillers to captivating memoirs, this week new releases offer something for everyone</p>
-      <button className=" max-w-[30%] py-2 bg-[#FFCE1A]">Subscribe</button>
+      <button className=" max-w-[30%] py-2 bg-[#FFCE1A] items-cente justify-center justify-items-center">Subscribe</button>
       </div>
       <div>
         <img src="/src/assets/Book Store/Animation.png" className=" w-[486px] h-[378px]" alt="" />
