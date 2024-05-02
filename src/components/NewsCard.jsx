945264869f620f945c224f0b76927104d5d1eb9e
jusish <ishimwejustin67@outlook.com>
2024-05-02 21:55:06 +0200

    News card updated

--- src/components/NewsCard.jsx
+++ src/components/NewsCard.jsx
@@ -3,8 +3,8 @@ import MultipleItems3 from "./Slider3"
 
 const News = () => {
   return (
-    <div className=" mt-20 text-start flex flex-col gap-10">
-      <h2>News</h2>
+    <div className="flex flex-col gap-10 mx-10 mt-20 text-start">
+      <h2 className="text-2xl font-semibold">News</h2>
       <div>
         <MultipleItems3/>
       </div>
